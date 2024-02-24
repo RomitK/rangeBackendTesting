1cc3415d353180977369afb4ed4b81f022bfafff
romit <romit@range.ae>
2024-02-24 19:17:14 +0400

    cros issue

--- config/cors.php
+++ config/cors.php
@@ -19,7 +19,7 @@ return [
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['http://localhost:9999', 'https://range-frontend-omega.vercel.app'],
+    'allowed_origins' => ['*'],
 
     'allowed_origins_patterns' => [],
 
