5c9d1b45c6bbb7a2a5e3120a993ef4975a173d83
romit <romit@range.ae>
2024-03-06 05:44:14 +0000

    media order by publish date

--- app/Http/Controllers/API/MediaController.php
+++ app/Http/Controllers/API/MediaController.php
@@ -22,7 +22,7 @@ class MediaController extends Controller
     {
         try {
 
-            $collection = Article::active()->approved()->orderByRaw('ISNULL(publish_at)')->orderBy('publish_at');
+            $collection = Article::active()->approved()->orderByRaw('ISNULL(publish_at)')->orderBy('publish_at', 'desc');
             //$collection = Article::active()->approved()->latest();
             $medias = clone $collection;
             $news = clone $collection;
