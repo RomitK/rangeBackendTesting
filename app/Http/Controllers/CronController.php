8c73f10b09580aaf4464e7d6aadf60ae3c36fed9
romit <romit@range.ae>
2024-07-29 12:28:22 +0000

    working on the properties qr and permit number

--- app/Http/Controllers/CronController.php
+++ app/Http/Controllers/CronController.php
@@ -39,7 +39,7 @@ class CronController extends Controller
     {
         DB::beginTransaction();
         try {
-            $projects = Project::skip(300)->take(40)->orderBy('id', 'asc')->get();
+            $projects = Project::skip(340)->take(40)->orderBy('id', 'asc')->get();
            
             foreach($projects as $project){
                 $properties = Property::whereIn('project_id', [$project->id])->get();
