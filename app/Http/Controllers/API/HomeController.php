a75b21f8be8d2ca3e5bc704a2b86ca68562c0632
aqsa <aqsa@xpertise.ae>
2024-08-15 05:24:55 +0000

    home page data cache

--- app/Http/Controllers/API/HomeController.php
+++ app/Http/Controllers/API/HomeController.php
@@ -447,7 +447,7 @@ echo $curl_scraped_page;
     {
         try {
 
-            return $this->success('Home Data', Cache::remember('homeData', 0 * 0 * 0, function () {
+            return $this->success('Home Data', Cache::remember('homeData', 24 * 60 * 60, function () {
                 // $communities = HomeCommunitiesResource::collection(Community::active()->approved()->home()->limit(12)->orderByRaw('ISNULL(communityOrder)')->orderBy('communityOrder', 'asc')->get() );
                 $communities =  HomeCommunitiesResource::collection(DB::table('communities')
                     ->select('name', 'slug', 'banner_image', 'id')
